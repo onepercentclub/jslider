18ddbd9fd2e2b6cae712b6bacc0b35c98116869a
David Børresen <db@warmsys.com>
2014-01-31 11:08:25 +0100

    fixed issues with non-working grunt build proccess

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        uglify : {
+            options : {
+                mangle : {
+                    except : ['jQuery']
+                }
+            },
+            main: {
+                options: {
+                    sourceMap: 'dist/jquery.slider.min.map'
+                },
+                files : {
+                    'dist/jquery.slider.min.js' : [
+                        'js/jshashtable-2.1_src.js',
+                        'js/tmpl.js',
+                        'js/jquery.dependClass-0.1.js',
+                        'js/jquery.numberformatter-1.2.3.js',
+                        'js/draggable-0.2.js',
+                        'js/jquery.slider.js'
+                    ]
+                }
+            }
+        },
+        cssmin : {
+            compress : {
+                files : {
+                    "dist/jquery.slider.min.css" : ['css/jslider.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+};
